0cfca177049251c72f99d85bdab458590cf9ede6
Zhiyan Foo <zhiyanfoo@gmail.com>
2020-09-22 00:53:06 +0800

    change run_dev.sh

--- backend/deploy/run_dev.sh
+++ backend/deploy/run_dev.sh
@@ -3,7 +3,7 @@ set -eu
 cat docker-compose.tpl.yml \
   | sigil \
   'postgres_password=dev' \
-  'app_origin=localhost:80' \
+  'app_origin=localhost' \
   > docker-compose.yml
 cat docker-compose.yml
 docker stack deploy editor --compose-file docker-compose.yml
