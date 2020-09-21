3c0cd2f1ce11004b35795775f4aba1bdcd2318ee
Zhiyan Foo <zhiyanfoo@gmail.com>
2020-09-22 02:00:42 +0800

    prettify

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const hostname = window.location.hostname
-console.log(hostname)
+const hostname = window.location.hostname;
+console.log(hostname);
 export const HTTPS_BASE_URL = `http://${hostname}:5001`;
 export const WEBSOCKET_BASE_URL = `ws://${hostname}:5000`;
